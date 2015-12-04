7b90c889b7e740b2de4946048cf0e9d222c3ba05
Teo Qi Xuan <teoqixuan@hotmail.com>
2015-12-04 22:27:33 +0800

    Initial Commit

---
+++ desktop/src/sanoke/qx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sanoke.qx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sanoke.qx.Sanoke;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sanoke(), config);
+	}
+}
---
+++ html/src/sanoke/qx/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package sanoke.qx.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import sanoke.qx.Sanoke;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Sanoke();
+        }
+}
